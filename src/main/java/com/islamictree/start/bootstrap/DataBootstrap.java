4a3ab33cadd8f92dc5bd4e9a47922f91bae9beaf
Hasham M Khan <hasham.m.khan@outlook.com>
2025-08-12 02:17:29 -0500

    Add dummy data. Closes #2.

---
+++ src/main/java/com/islamictree/start/bootstrap/DataBootstrap.java
@@ -0,0 +1,13 @@
+package com.islamictree.start.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataBootstrap implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
