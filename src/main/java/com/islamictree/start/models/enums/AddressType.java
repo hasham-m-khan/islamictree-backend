1f7cfb875fd137b8fe2f0db7d11b4ee346e270a8
Hasham M Khan <hasham.m.khan@outlook.com>
2025-08-21 16:27:04 -0500

    Updates AddressType enum toString and adds fromString methods.

--- src/main/java/com/islamictree/start/models/enums/AddressType.java
+++ src/main/java/com/islamictree/start/models/enums/AddressType.java
@@ -1,5 +1,19 @@
 package com.islamictree.start.models.enums;
 
 public enum AddressType {
-    RESIDENTIAL, BUSINESS, PO_BOX, OTHER
+    RESIDENTIAL, BUSINESS, PO_BOX, OTHER;
+
+    @Override
+    public String toString() {
+        return this.name();
+    }
+
+    public static AddressType fromString(String value) {
+        for (AddressType type : AddressType.values()) {
+            if (type.name().equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Unknown AddressType: " + value);
+    }
 }
