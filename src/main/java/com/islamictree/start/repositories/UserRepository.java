7cef1197cc78243226af79e3cab677eef5db5c0d
Hasham M Khan <hasham.m.khan@outlook.com>
2025-08-23 08:40:26 -0500

    Updates User repository to find user with email.

--- src/main/java/com/islamictree/start/repositories/UserRepository.java
+++ src/main/java/com/islamictree/start/repositories/UserRepository.java
@@ -4,8 +4,11 @@ import com.islamictree.start.models.User;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface UserRepository extends R2dbcRepository<User, Long> {
     Flux<User> findByAddressId(Long addressId);
+
+    Mono<User> findByEmail(String email);
 }
