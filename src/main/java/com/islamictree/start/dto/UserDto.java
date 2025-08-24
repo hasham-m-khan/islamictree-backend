10d38e5b007ec5a0d759ad34bee2f07a1812464a
Hasham M Khan <hasham.m.khan@outlook.com>
2025-08-24 11:26:30 -0500

    Implements various constructors for UserDto for unit testing.

--- src/main/java/com/islamictree/start/dto/UserDto.java
+++ src/main/java/com/islamictree/start/dto/UserDto.java
@@ -22,6 +22,41 @@ public class UserDto {
     private Timestamp createdAt;
     private Timestamp updatedAt;
 
+    public UserDto(Long id, String firstName, String lastName, String email, String passwordHash,
+                   String phoneNumber, Boolean isActive, Boolean emailVerified) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.phoneNumber = phoneNumber;
+        this.isActive = isActive;
+        this.emailVerified = emailVerified;
+    }
+
+    public UserDto(String firstName, String lastName, String email, String passwordHash, String phoneNumber,
+                   Long addressId, Boolean isActive, Boolean emailVerified) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.phoneNumber = phoneNumber;
+        this.addressId = addressId;
+        this.isActive = isActive;
+        this.emailVerified = emailVerified;
+    }
+
+    public UserDto(String firstName, String lastName, String email, String passwordHash, String phoneNumber,
+                   Boolean isActive, Boolean emailVerified) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.phoneNumber = phoneNumber;
+        this.isActive = isActive;
+        this.emailVerified = emailVerified;
+    }
+
     @Override
     public String toString() {
         return "UserDto{" +
