cd1ef1ea607759f615532d73042afe40461e5104
Hasham M Khan <hasham.m.khan@outlook.com>
2025-08-28 17:04:15 -0500

    Creates author_tag tables. Closes #40.

---
+++ src/main/resources/db/test-data/V110__Insert_test_author_article.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO author_tag (author_id, tag_id) VALUES (1, 1);
+INSERT IGNORE INTO author_tag (author_id, tag_id) VALUES (1, 2);
+INSERT IGNORE INTO author_tag (author_id, tag_id) VALUES (2, 4);
+INSERT IGNORE INTO author_tag (author_id, tag_id) VALUES (3, 4);
