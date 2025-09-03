41fc4c90638c7ef29d7cde383b68f55f469e3770
Hasham M Khan <hasham.m.khan@outlook.com>
2025-09-02 22:48:31 -0500

    Updates article table with data. Closes #66.

--- src/main/resources/db/test-data/V106__Insert_test_articles.sql
+++ src/main/resources/db/test-data/V106__Insert_test_articles.sql
@@ -1,18 +1,41 @@
-INSERT IGNORE INTO articles (id, title, blurb, content) VALUES (
+INSERT IGNORE INTO articles (id, title, blurb, content, date_published, image_url) VALUES (
     1,
     'Rich Dad Poor Dad',
     'Rich Dad Poor Dad is a 1997 book written by Robert T. Kiyosaki and Sharon Lechter. It advocates the importance of financial literacy, financial independence and building wealth through investing in assets, real estate investing, starting and owning businesses, as well as increasing one''s financial intelligence',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    '2024-04-03',
+    '/img/articles/sample_1.jpg'
 );
-INSERT IGNORE INTO articles (id, title, blurb, content) VALUES (
+INSERT IGNORE INTO articles (id, title, blurb, content, date_published, image_url) VALUES (
     2,
     'Dune: Awakening',
     'Unlock the secrets of Arrakis with Dune: Awakening – The Ultimate Game Guide! Whether you’re a fledgling survivor struggling against the relentless desert or a battle-hardened faction leader vying for control of the spice-rich sands, this comprehensive guide is your indispensable companion through every perilous step of the journey.',
-    'Dive deep into the brutal, beautiful world of Dune: Awakening—master survival tactics to conquer the harsh desert environment, perfect your combat skills against ruthless enemies, and build impregnable bases that withstand the fiercest assaults. Discover advanced strategies for resource control, faction diplomacy, and large-scale PvP warfare that will elevate your gameplay from mere survival to total domination. From the first cautious steps on the dunes to epic endgame battles that shape the future of Arrakis, this book breaks down complex mechanics and hidden secrets with expert clarity. Learn how to harness the power of the desert, command specialized character builds, and forge alliances that tip the balance of power.'
+    'Dive deep into the brutal, beautiful world of Dune: Awakening—master survival tactics to conquer the harsh desert environment, perfect your combat skills against ruthless enemies, and build impregnable bases that withstand the fiercest assaults. Discover advanced strategies for resource control, faction diplomacy, and large-scale PvP warfare that will elevate your gameplay from mere survival to total domination. From the first cautious steps on the dunes to epic endgame battles that shape the future of Arrakis, this book breaks down complex mechanics and hidden secrets with expert clarity. Learn how to harness the power of the desert, command specialized character builds, and forge alliances that tip the balance of power.',
+    '2024-04-03',
+    '/img/articles/sample_2.jpg'
 );
-INSERT IGNORE INTO articles (id, title, blurb, content) VALUES (
+INSERT IGNORE INTO articles (id, title, blurb, content, date_published, image_url, source) VALUES (
     3,
     'The Expanse: Leviathan Wakes',
     'From a New York Times best-selling and Hugo award-winning author comes a modern masterwork of science fiction, introducing a captain, his crew, and a detective as they unravel a horrifying solar system wide conspiracy that begins with a single missing girl.',
-    'Humanity has colonized the solar system - Mars, the Moon, the Asteroid Belt and beyond - but the stars are still out of our reach. Jim Holden is XO of an ice miner making runs from the rings of Saturn to the mining stations of the Belt. When he and his crew stumble upon a derelict ship, the Scopuli, they find themselves in possession of a secret they never wanted. A secret that someone is willing to kill for - and kill on a scale unfathomable to Jim and his crew. War is brewing in the system unless he can find out who left the ship and why.'
+    '<p>A hundred and fifty years before, when the parochial disagreements between Earth and Mars had been on the verge of war, the Belt had been a far horizon of tremendous mineral wealth beyond viable economic reach, and the outer planets had been beyond even the most unrealistic corporate dream. Then Solomon Epstein had built his little modified fusion drive, popped it on the back of his three-man yacht, and turned it on. With a good scope, you could still see his ship going at a marginal percentage of the speed of light, heading out into the big empty. The best, longest funeral in the history of mankind. Fortunately, he’d left the plans on his home computer. The Epstein Drive hadn’t given humanity the stars, but it had delivered the planets.</p>
+<p>Three-quarters of a kilometer long, a quarter of a kilometer wide—roughly shaped like a fire hydrant—and mostly empty space inside, the Canterbury was a retooled colony transport. Once, it had been packed with people, supplies, schematics, machines, environment bubbles, and hope. Just under twenty million people lived on the moons of Saturn now. The Canterbury had hauled nearly a million of their ancestors there. Forty-five million on the moons of Jupiter. One moon of Uranus sported five thousand, the farthest outpost of human civilization, at least until the Mormons finished their generation ship and headed for the stars and freedom from procreation restrictions.</p>
+<p>And then there was the Belt.</p>
+<p>If you asked OPA recruiters when they were drunk and feeling expansive, they might say there were a hundred million in the Belt. Ask an inner planet census taker, it was nearer to fifty million. Anyway you looked, the population was huge and needed a lot of water.</p>
+<p>So now the Canterbury and her dozens of sister ships in the Pur\’n\’Kleen Water Company made the loop from Saturn’s generous rings to the Belt and back hauling glaciers, and would until the ships aged into salvage wrecks.</p>
+<p>Jim Holden saw some poetry in that.<br></p>
+<q>Holden?</q>
+<p>He turned back to the hangar deck. Chief Engineer Naomi Nagata towered over him. She stood almost two full meters tall, her mop of curly hair tied back into a black tail, her expression halfway between amusement and annoyance. She had the Belter habit of shrugging with her hands instead of her shoulders.</p>
+<p><q>Holden, are you listening, or just staring out the window?</q><br>
+<q>There was a problem,</q> Holden said. <q>And because you’re really, really good, you can fix it even though you don’t have enough money or supplies.</q></p>
+<p>Naomi laughed.<br>
+<q>So you weren’t listening,</q> she said.<br>
+<q>Not really, no.</q><br>
+<q>Well, you got the basics right anyhow. Knight\’s landing gear isn\’t going to be good in atmosphere until I can get the seals replaced. That going to be a problem?</q><br>
+<q>I’ll ask the old man,</q> Holden said. <q>But when’s the last time we used the shuttle in atmosphere?</q><br>
+<q>Never, but regs say we need at least one atmo-capable shuttle.</q></p>
+<p><q>Hey, Boss!</q> Amos Burton, Naomi’s earthborn assistant, yelled from across the bay. He waved one meaty arm in their general direction. He meant Naomi. Amos might be on Captain McDowell’s ship; Holden might be executive officer; but in Amos Burton’s world, only Naomi was boss.</p>',
+    '2024-04-03',
+    '/img/articles/sample_3.jpg',
+    'http://islamictree.com'
 );
